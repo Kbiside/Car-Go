1570f4c7ba01c8a94cace569cd398cb9edb6c8bb
fribrugh <badmvalentina@gmail.com>
2025-04-16 05:10:45 +0000

    5454

--- adminsite/src/main.tsx
+++ adminsite/src/main.tsx
@@ -5,11 +5,13 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import { ClientAddPage } from "./pages/AddClient.tsx";
 import { MantineProvider } from "@mantine/core";
 import { theme } from "./theme.ts";
+import { DashboardPage } from "./pages/Dashboard.tsx";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <MantineProvider theme={theme}>
   <BrowserRouter>
     <Routes>
-      <Route path="dashboard" element = {<ClientAddPage/>}/>
+      <Route path="addclient" element = {<ClientAddPage/>}/>
+      <Route path="dashboard" element = {<DashboardPage/>}/>
     </Routes>
   </BrowserRouter>
   </MantineProvider>
